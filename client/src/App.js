34c5e24886b5515b5668c872c3b00e84e1bed83d
Robert Dragoi <robertdragoi20@gmail.com>
2021-10-24 23:21:52 +0300

    Change name for containers

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-        <Route path="/register">
+        <Route exact path="/register">
           <Register />
         </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,24 +3,24 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="container-left">
+    <div className="navbar-container">
+      <div className="navbar-container-left">
         <div className="item">
           <NavLink className="link" to="/register">
             Register
           </NavLink>
         </div>
         <div className="item">
-          <NavLink className="link" to="/">
+          <NavLink className="link" to="/login">
             Login
           </NavLink>
         </div>
       </div>
 
-      <div className="container-right">
+      <div className="navbar-container-right">
         <div>
           <div className="item">
-            <NavLink className="link" to="/">
+            <NavLink className="link" to="/auctions">
               Auctions
             </NavLink>
           </div>
