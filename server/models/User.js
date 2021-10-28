24644e7b9cc2cef33f9370670e113b048fa576f7
Robert Dragoi <robertdragoi20@gmail.com>
2021-10-28 21:34:40 +0300

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+let UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true, unique: true },
+  address: { type: String, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, required: true, default: Date.now },
+});
+module.exports = mongoose.model("User", UserSchema);
