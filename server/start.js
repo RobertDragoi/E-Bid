d5f3e23c14107414026420197d1ffe73b7ff06ee
Robert Dragoi <robertdragoi20@gmail.com>
2021-11-02 21:23:15 +0200

    Add bodyparser

--- server/start.js
+++ server/start.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
-
+const { ConnectDataBase } = require("./database");
+require("dotenv").config();
+ConnectDataBase();
+app.use(express.json({ extended: false }));
+app.use("/api/user", require("./routes/user-routes"));
 app.listen(8080, () => console.log("listening on port 8080"));
