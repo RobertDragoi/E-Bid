1cda551e03db08990d7b6ef9434541827a82b4a5
Robert Dragoi <robertdragoi20@gmail.com>
2021-11-03 12:25:27 +0200

    Add logout button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Navbar.scss";
 const Navbar = () => {
   const { isAuthenticated, user } = useSelector((state) => state.user);
   return (
     <div className="navbar-container">
       <div className="navbar-container-left">
-        <div className="navbar-item">
-          {isAuthenticated ? (
-            <NavLink className="navbar-link" to="/register">
-              {user?.name}
-            </NavLink>
-          ) : (
+        {isAuthenticated ? (
+          <>
+            <div className="navbar-item">
+              <NavLink className="navbar-link" to="/register">
+                {user?.name}
+              </NavLink>
+            </div>
+            <div className="navbar-item">
+              <buttom
+                onClick={() => console.log("logout")}
+                className="navbar-button"
+              >
+                <FontAwesomeIcon color="white" icon={faSignOutAlt} />
+              </buttom>
+            </div>
+          </>
+        ) : (
+          <div className="navbar-item">
             <NavLink className="navbar-link" to="/register">
               Register
             </NavLink>
-          )}
-        </div>
+          </div>
+        )}
       </div>
 
       <div className="navbar-container-right">
