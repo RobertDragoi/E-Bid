49debedbc2af417d31f7c4259a2acb37abc55141
Robert Dragoi <robertdragoi20@gmail.com>
2021-11-03 10:07:28 +0200

    Install cors

--- server/start.js
+++ server/start.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { ConnectDataBase } = require("./database");
 require("dotenv").config();
 ConnectDataBase();
+app.use(cors());
 app.use(express.json({ extended: false }));
 app.use("/api/user", require("./routes/user-routes"));
 app.listen(8080, () => console.log("listening on port 8080"));
