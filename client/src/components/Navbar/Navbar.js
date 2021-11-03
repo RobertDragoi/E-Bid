2cccf731d5b09bf43e561db154aa9db83bba783b
Robert Dragoi <robertdragoi20@gmail.com>
2021-11-03 10:42:44 +0200

    Improve navbar functionality

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Navbar.scss";
 const Navbar = () => {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
   return (
     <div className="navbar-container">
       <div className="navbar-container-left">
         <div className="navbar-item">
-          <NavLink className="navbar-link" to="/register">
-            Register
-          </NavLink>
+          {isAuthenticated ? (
+            <NavLink className="navbar-link" to="/register">
+              {user?.name}
+            </NavLink>
+          ) : (
+            <NavLink className="navbar-link" to="/register">
+              Register
+            </NavLink>
+          )}
         </div>
       </div>
 
