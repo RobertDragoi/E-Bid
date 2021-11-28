644f05aaa8500448433e2283c0bfc64abbde17f0
Robert Dragoi <robertdragoi20@gmail.com>
2021-11-28 17:40:37 +0200

    Add dropdown menu for navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSignOutAlt,
+  faAlignJustify,
+  faUserAlt,
+  faUserPlus,
+  faStamp,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch } from "react-redux";
 import { logoutOperation } from "../../state/operations/userOperations";
@@ -11,40 +17,50 @@ const Navbar = () => {
   const { isAuthenticated, user } = useSelector((state) => state.user);
   return (
     <div className="navbar-container">
-      <div className="navbar-container-left">
+      <div className="navbar-item">
+        <div className="navbar-item-icon">
+          <FontAwesomeIcon color="white" icon={faAlignJustify} />
+        </div>
         {isAuthenticated ? (
-          <>
-            <div className="navbar-item">
+          <div className="navbar-item-content">
+            <div className="navbar-item-content-item">
               <NavLink className="navbar-link" to="/profile">
+                <FontAwesomeIcon color="white" icon={faUserAlt} />
                 {user?.name}
               </NavLink>
             </div>
-            <div className="navbar-item">
-              <button
-                onClick={() => dispatch(logoutOperation)}
-                className="navbar-button"
-              >
-                <FontAwesomeIcon color="white" icon={faSignOutAlt} />
-              </button>
+            <div className="navbar-item-content-item">
+              <NavLink className="navbar-link" to="/auctions">
+                <FontAwesomeIcon color="white" icon={faStamp} />
+                Auctions
+              </NavLink>
             </div>
-          </>
+            <div
+              className="navbar-item-content-item"
+              onClick={() => dispatch(logoutOperation)}
+            >
+              <div className="navbar-link">
+                <FontAwesomeIcon color="white" icon={faSignOutAlt} />
+                Log out
+              </div>
+            </div>
+          </div>
         ) : (
-          <div className="navbar-item">
-            <NavLink className="navbar-link" to="/register">
-              Register
-            </NavLink>
+          <div className="navbar-item-content">
+            <div className="navbar-item-content-item">
+              <NavLink className="navbar-link" to="/register">
+                <FontAwesomeIcon color="white" icon={faUserPlus} />
+                Register
+              </NavLink>
+            </div>
+            <div className="navbar-item-content-item">
+              <NavLink className="navbar-link" to="/auctions">
+                <FontAwesomeIcon color="white" icon={faStamp} />
+                Auctions
+              </NavLink>
+            </div>
           </div>
         )}
-      </div>
-
-      <div className="navbar-container-right">
-        <div>
-          <div className="navbar-item">
-            <NavLink className="navbar-link" to="/auctions">
-              Auctions
-            </NavLink>
-          </div>
-        </div>
       </div>
     </div>
   );
