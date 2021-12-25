620d0d1eb435c76c46441efd32dc9ebdeaa40e26
Robert Dragoi <robertdragoi20@gmail.com>
2021-12-25 17:32:39 +0200

    Update landing

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
 import "./LandingPage.scss";
 const LandingPage = () => {
   const history = useHistory();
+  const { isAuthenticated } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push("/auctions");
+    }
+  });
   return (
     <div className="landing-background">
       <div className="landing-background-form">
@@ -10,12 +17,21 @@ const LandingPage = () => {
           <h3 className="landing-background-title">
             You want to start an auction? Here is the place to do it
           </h3>
-          <button
-            onClick={() => history.push("/auctions")}
-            className="landing-background-landing-button"
-          >
-            Auctions
-          </button>
+          {isAuthenticated ? (
+            <button
+              onClick={() => history.push("/auctions")}
+              className="landing-background-landing-button"
+            >
+              Auctions
+            </button>
+          ) : (
+            <button
+              onClick={() => history.push("/register")}
+              className="landing-background-landing-button"
+            >
+              Register
+            </button>
+          )}
         </div>
         <div></div>
       </div>
